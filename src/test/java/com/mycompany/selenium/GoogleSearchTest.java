1c2687783d4e4361edc39f654b6afaf9f800b1a4
ilja <ilja.pavlovs>
2017-10-19 01:34:21 +0300

    initial commit

---
+++ src/test/java/com/mycompany/selenium/GoogleSearchTest.java
@@ -0,0 +1,37 @@
+package com.mycompany.selenium;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+public class GoogleSearchTest{
+
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver-v2.30-win32/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("http://www.google.com");
+    }
+
+    @Test
+    public void testSearch() throws Exception {
+        WebElement element = driver.findElement(By.name("q"));
+        element.sendKeys("cheese");
+        element.submit();
+        assertTrue(driver.getTitle().contains("cheese"));
+    }
+
+    @AfterMethod
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
+}
