2a3a90bb4a506da86f83c45f9314bc834733c1d8
roman.krivosheyev <100rm500@gmail.com>
2017-10-28 15:18:19 +0300

    Homework Task 1+2 v.1.2

--- src/test/java/com/mycompany/selenium/SScomTest.java
+++ src/test/java/com/mycompany/selenium/SScomTest.java
@@ -4,18 +4,16 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.util.*;
 import java.math.BigDecimal;
 import java.util.List;
-
 import static org.testng.Assert.assertTrue;
-
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
