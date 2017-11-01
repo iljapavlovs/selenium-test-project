720564725901d635756a66e84ab54d7b7f9d1f48
S1EAMK <4Sisma123sr>
2017-11-02 01:03:34 +0200

    added allure2

--- src/test/java/com/mycompany/selenium/GoogleSearchTest.java
+++ src/test/java/com/mycompany/selenium/GoogleSearchTest.java
@@ -1,20 +1,19 @@
 package com.mycompany.selenium;
 
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import io.qameta.allure.*;
+import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertTrue;
 
+@Feature("Google Search")
 public class GoogleSearchTest{
 
     private WebDriver driver;
@@ -29,16 +28,52 @@ public class GoogleSearchTest{
         driver.get("http://www.google.com");
     }
 
+    @AfterMethod
+    public void tearDown(ITestResult testResult) throws Exception {
+        if (testResult.getStatus() == ITestResult.FAILURE) {
+            System.out.println("Test failed: " + testResult.getMethod().getMethodName());
+            takeScreenShot(testResult.getMethod().getMethodName());
+        }
+
+        driver.quit();
+    }
+
     @Test
-    public void testSearch() throws Exception {
+    @Story("Cheese Search")
+    @Issue("ALR-123")
+    @TmsLink("TMS-123")
+    public void testSearchCheese() throws Exception {
         WebElement element = driver.findElement(By.name("q"));
         element.sendKeys("cheese", Keys.ENTER);
         new WebDriverWait(driver, 3).until(ExpectedConditions.titleContains("cheese"));
         assertTrue(driver.getTitle().contains("cheese"));
     }
 
-    @AfterMethod
-    public void tearDown() throws Exception {
-        driver.quit();
+
+    @Test
+    @Flaky
+    @Description("Some detailed test description")
+    @Story("Apple Search")
+    @Severity(SeverityLevel.CRITICAL)
+
+    @Issues({
+            @Issue("MYISSUE-1"),
+            @Issue("MYISSUE-2")
+    })
+    @TmsLink("TMS-123")
+    public void testSearchApple() throws Exception {
+        WebElement element = driver.findElement(By.name("q"));
+        element.sendKeys("apple", Keys.ENTER);
+        new WebDriverWait(driver, 3).until(ExpectedConditions.titleContains("apple"));
+        assertTrue(false, "Purposefully fail this test in order to generate screenshot");
     }
+
+
+    @Attachment(value = "Failure in method {0}", type = "image/png")
+    private byte[] takeScreenShot(String methodName) {
+        System.out.println("Taking screenshot for failed test: " + methodName);
+        return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+    }
+
+
 }
