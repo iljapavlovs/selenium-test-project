9d9ed612f0548dd12491424e94900d862fa5e20a
roman.krivosheyev <100rm500@gmail.com>
2017-11-02 09:57:16 +0300

    Homework Task 1+2 v.3.0

---
+++ src/test/java/com/mycompany/selenium/tests/SomethingTest.java
@@ -0,0 +1,54 @@
+package com.mycompany.selenium.tests;
+
+
+import com.mycompany.selenium.pageobejcts.TopMenuBar;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class SomethingTest {
+
+
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver-v2.33-win32/chromedriver.exe");
+        driver = new ChromeDriver();
+
+
+        driver.manage().window().maximize();
+        driver.get("http://ss.com");
+    }
+
+    @AfterMethod
+    public void teardown() throws Exception {
+
+        driver.quit();
+    }
+
+    @Test
+    public void testSmth() throws Exception {
+        TopMenuBar topMenuBar = new TopMenuBar(driver);
+        topMenuBar.changeLanguage();
+
+        assertEquals(topMenuBar.getPageHeaderHeadText(), "ОБЪЯВЛЕНИЯ");
+
+
+    }
+
+    @Test
+    public void testSmth2() throws Exception {
+        TopMenuBar topMenuBar = new TopMenuBar(driver);
+        topMenuBar.changeLanguage();
+        topMenuBar.goToSearch();
+
+
+    }
+
+}
