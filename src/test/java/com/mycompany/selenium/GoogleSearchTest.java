ac7c41f735d19f6526554a7ceb955f7bf34195a2
sisternika <sisternika@yandex.ru>
2017-11-02 12:23:33 +0300

    Task 1 + Sorting from Task 2

--- src/test/java/com/mycompany/selenium/GoogleSearchTest.java
+++ src/test/java/com/mycompany/selenium/GoogleSearchTest.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
@@ -21,10 +20,10 @@ public class GoogleSearchTest{
 
     @BeforeMethod
     public void setUp() throws Exception {
-//        System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver-v2.33-win32/chromedriver.exe");
-//        driver = new ChromeDriver();
-        System.setProperty("webdriver.gecko.driver", "src/test/resources/drivers/geckodriver-v0.17.0-win32/geckodriver.exe");
-        driver = new FirefoxDriver();
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver-v2.33-win32/chromedriver.exe");
+        driver = new ChromeDriver();
+//        System.setProperty("webdriver.gecko.driver", "src/test/resources/drivers/geckodriver-v0.17.0-win32/geckodriver.exe");
+//        driver = new FirefoxDriver();
 
         driver.get("http://www.google.com");
     }
